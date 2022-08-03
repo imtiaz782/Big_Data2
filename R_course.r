e09f762c8fac9806c2761e8c2d507f0dc0d64bfc
Imtiaz Ali <imtiazak782@gmail.com>
2022-08-04 01:00:12 +0500

    R course from beignning

---
+++ R_course.r
@@ -0,0 +1,33 @@
+plot(1:10)
+5+5
+5
+1
+for (x in 1:10) {
+  print(x)
+}
+## Varaible
+name <- "imtiaz"  # ' name 'is varaible and 'imtiaz' ia value
+print(name)
+"imtiaz" -> # other way of code
+print(name)
+age = 20  #  ' = ' or ' ->' or same
+print(age)
+## To combine both text and a variable, R uses comma (,): 
+text <- "awesome"
+paste("R is" , text) ## here putting text result "awesome" text like a fullblank
+ name <- "imtiaz ali"
+paste("my name is " , name)
+score <- 70
+paste("my usat score is " , score)
+book = "Quran"
+paste("my favrit book is" , book) ## (,) comma combine the value
+text1 <- "R is"
+text2 <- "awesome"
+paste(text1,text2) ## proces same to above proces
+progmm = "R is best programming  "
+languge = "language"
+paste(progmm , languge)
+party <- "PTI is the "
+value <- "6th large party "
+world <-  "of world"
+paste(party,value,world)
